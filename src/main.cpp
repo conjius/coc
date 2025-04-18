1b97250186bfecbfacd54a5b2963a48afe0df2eb
danny <dannypriymak@gmail.com>
2025-04-18 16:52:41 +0300

    add frame counter

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,23 @@
 #include "Game.h"
 #include "raylib.h"
 
-#define WINDOW_WIDTH 1280
-#define WINDOW_HEIGHT 720
+#define WINDOW_WIDTH 500
+#define WINDOW_HEIGHT 500
 
 int main() {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Clash of Cultures");
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
         const auto game = std::make_unique<Game>();
+        uint64_t frame = 0;
 
         while (!game->isOver()) {
             BeginDrawing();
             ClearBackground(RAYWHITE);
             game->doNextTurn();
+            DrawText(TextFormat("%i", frame), 20, 20, 30, BLACK);
             EndDrawing();
+            frame++;
         }
     }
     CloseWindow();
